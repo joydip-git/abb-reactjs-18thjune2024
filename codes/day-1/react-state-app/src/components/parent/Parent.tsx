17cd222a9614bb40c1fa6f9a68f0d24fb1bd827c
Joydip <joydip.mondal@gmail.com>
2024-06-19 09:54:12 +0530

    rect-state-app of day-1 updated

--- codes/day-1/react-state-app/src/components/parent/Parent.tsx
+++ codes/day-1/react-state-app/src/components/parent/Parent.tsx
@@ -1,16 +1,38 @@
 import { useState } from "react"
 import Nested from "../nested/Nested"
 
+type StateStype = { count: number }
+
 const Parent = () => {
     console.log('parent rendered');
 
     const [data, setData] = useState<number>(0)
+    const [value, setValue] = useState<StateStype>({ count: 0 })
 
     const dataHandler = () => {
-        setData(100)
+        //setData(100)
+        setData(
+            (currentDataState) => currentDataState + 1
+        )
+    }
+    const valueStateHandler = () => {
+        setValue(
+            (currentValueState) => {
+                return { count: currentValueState.count + 1 }
+            }
+        )
     }
     return (
         <div>
+            <div>
+                <span>Value: &nbsp;{value.count}</span>
+                <br />
+                <button type="button"
+                    onClick={valueStateHandler}>
+                    Increase Value State
+                </button>
+            </div>
+            <br />
             <Nested
                 value={data}
                 valueHandler={dataHandler}
